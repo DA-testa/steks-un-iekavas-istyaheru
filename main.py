57d2d48fdd4ed2c8bf5b55a50837f7739ef8d12c
Ēriks Lijurovs <eriks.lijurovs@edu.rtu.lv>
2023-02-18 23:57:20 +0200

    Noņemts nevajadzīgs print

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from collections import namedtuple
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 def are_matching(left, right):
-    print(left, right)
     return (left + right) in ["()", "[]", "{}"]
 
 def find_mismatch(text):
